51561ca249ae829fc005e1ab83574613922cf959
Alex Hagge and John Dow <john@>
2016-05-25 14:32:23 -0700

    applied underscore function

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,10 +1,21 @@
+
+
 //API logic
 var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=1').then(function(response){
   return dinoAnswer = response;
 });
 
 //Business logic
+
+//Convert word into array
 dinoSplit = function(dinoAnswer){
   dinoArray = dinoAnswer.split("", dinoAnswer.length - 3);
-  return dinoArray;
+  var x = dinoArray.join("");
+  var y = dinoReplace(x);
+  return y;
+}
+
+//Replace word array with underscores
+dinoReplace = function(x){
+  return dinoUnderScore = x.replace(/[a-z]/gi,"_ ");
 }
