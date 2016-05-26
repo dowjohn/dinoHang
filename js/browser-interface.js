793ccd0cefe3a9ad90718042ede1fb9f75ee58b2
Alex Hagge and John Dow <john@>
2016-05-26 09:23:53 -0700

    created methods for randomizing dinos

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
+  $('#guessDinos').hide();
+
+  $('#rightDino').hide();
+
   $('#form1').submit(function(event){
     event.preventDefault();
     $("#dinoHere").text(dinoAnswer);
+
+    setTimeout(dinoHide, 5000);
+
+    setInterval(showDinoInput, 5000);
   });
+
+  $('#form2').submit(function(event){
+    event.preventDefault();
+    var dinoNumber = $('#dinoInput').val();
+
+    if (dinoNumber <= dinoCount()) {
+      $('#rightDino').show();
+    }
+  });
+
+
 
 });
--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,21 +1,23 @@
-
-
-//API logic
-var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=1').then(function(response){
+var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=' + Math.floor((Math.random() * 100) + 1)).then(function(response) {
   return dinoAnswer = response;
 });
 
 //Business logic
 
-//Convert word into array
-dinoSplit = function(dinoAnswer){
-  dinoArray = dinoAnswer.split("", dinoAnswer.length - 3);
-  var x = dinoArray.join("");
-  var y = dinoReplace(x);
-  return y;
+dinoThunder = function(dinoAnswer) {
+  return dinoArray = dinoAnswer.split(" ");
+};
+
+//Set Time Out
+
+dinoHide = function() {
+  $("#dinoHere").hide();
 }
 
-//Replace word array with underscores
-dinoReplace = function(x){
-  return dinoUnderScore = x.replace(/[a-z]/gi,"_ ");
+showDinoInput = function() {
+  $('#guessDinos').show();
+}
+
+dinoCount = function() {
+  return dinoArray = dinoAnswer.split(" ").length;
 }
